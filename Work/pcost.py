cfeb066a9029d03755f9753dc2b1b4ad3203582c
BekkoCodes <bekko02@gmail.com>
2020-08-29 21:42:05 +0200

    Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('data/portfolio.csv', 'rt') as file:
+    next(file)
+    total_sum = 0
+    for line in file:
+        row = line.split(',')
+        total_sum += int(row[1]) * float(row[2])
+    print(f'Total cost {total_sum}')
