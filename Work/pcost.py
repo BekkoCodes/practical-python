fda429e258358cd65306a52380837104238078fc
BekkoCodes <bekko02@gmail.com>
2020-08-30 23:03:37 +0200

    Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,10 +2,16 @@
 #
 # Exercise 1.27
 
-with open('data/portfolio.csv', 'rt') as file:
-    next(file)
-    total_sum = 0
-    for line in file:
-        row = line.split(',')
-        total_sum += int(row[1]) * float(row[2])
-    print(f'Total cost {total_sum}')
+def portfolio_cost(filename):
+    """Calculates the total market share cost"""
+    with open(filename, 'rt') as file:
+        next(file)
+        total_sum = 0
+        for line in file:
+            row = line.split(',')
+            total_sum += int(row[1]) * float(row[2])
+        return total_sum
+
+
+cost = portfolio_cost('data/portfolio.csv')
+print(f'Total cost: {cost}')
