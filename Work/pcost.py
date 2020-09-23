e0cda72ce01b35780a7ffc6c3550314fff1a86f4
BekkoCodes <bekko02@gmail.com>
2020-09-23 22:34:16 +0200

    Exercise 2.16

--- Work/pcost.py
+++ Work/pcost.py
@@ -11,11 +11,15 @@ def portfolio_cost(filename):
     """Calculates the total market share cost"""
     with open(filename) as file:
         rows = csv.reader(file)
-        next(rows)
+        headers = next(rows)
         total_sum = 0
-        for row_number, row in enumerate(rows):
+        for row_number, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
             try:
-                total_sum += int(row[1]) * float(row[2])
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total_sum += nshares * price
+            # this catches errors in int() and float() conversions above
             except ValueError:
                 print(f'Row {row_number}: Bad row: {row}')
         return total_sum
